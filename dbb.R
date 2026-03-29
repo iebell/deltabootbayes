b827c491e2d7943ae15cfae85b21f7ead7cf36c4
iebell <ibell@vt.edu>
2026-03-29 17:06:01 -0400

    Added bootstrap function. Now trying to modify to remove issues with 0s and 1s.

--- dbb.R
+++ dbb.R
@@ -24,10 +24,9 @@ p1List <- lapply(sampleSize, function (x) {bSample(5000, x, 0.5)})
 p1Means <- lapply(p1List, function (y) {apply(y, 2, mean)})
 p1Delta <- p1Means
 for (i in 1:6) {
-  p1Delta[[i]] <- vapply(p1Means[[i]], 
-    function (x) {delta(sampleSize[i], x, 0.5)}, numeric(1))
+  p1Delta[[i]] <- sapply(p1Delta[[i]], 
+    function (x) {delta(sampleSize[i], x, 0.5)})
 }
-hist(p1Delta[[6]], breaks = 100)
 
 ##Problem 2, Delta Method
 s2 <- c(10, 30, 100, 1000)
@@ -36,18 +35,53 @@ sims <- expand.grid(
   n = s2,
   p = p2
 )
-p2List <- apply(sims, 1, function(x) {bSample(10, x[1], x[2])})
-p2Means <- lapply(p2list, function (y) {apply(y, 2, mean)})
-p2Exp <- p2Means
-for (r in 1:length(sims[,1])) {
-  
+# Samples generated, do not re-reun
+# p2List <- apply(sims, 1, function(x) {bSample(100, x[1], x[2])})
+saveRDS(p2List, file = "p2list.rds")
+p2ListPerm <- readRDS("p2list.rds")
+
+#Filter the lists to get 
+
+p2ListFiltered <-
+
+#Obtain the expectation and variances
+expectation <- function(x) {
+  mean(x)/(1-mean(x))
 }
-p2Var <-
+deltaVar <- function (y) {
+  (1/(1-mean(y)))^4 * var(y)
+}
+p2Exp <- lapply(p2ListPerm, function (y) {apply(y, 2, expectation)})
+p2Var <- lapply(p2ListPerm, function (y) {apply(y, 2, deltaVar)})
+
 
 ##Problem 2, Bootstrap
+
+
+bootSamples <- function (x, b) {
+    replicate(b, sample(x, length(x), replace = TRUE), simplify = TRUE)
+}
+
+
+bootstrapMeans <- function(x, b) {
   
+  samples <- bootSamples(x, b)
   
+#If any of the samples are all 1's, our estimates will be useless.
+#We replace any all one's column with a column of n-1 1's and one 0.
   
+  replace(bootSamples, which(colSums(samples) == nrow(samples), 
+                             c(r, ))
   
+  bMean <- function (y) {
+    y <- ifelse(is.infinite(expectation(y)), 10000, expectation(y))
+  }
+  
+  # Calculate the mean
+  bSampleMeans <- mean((apply(samples, 2, bMean)))
+  return(bSampleMeans)
+  
+}
+
   
   
